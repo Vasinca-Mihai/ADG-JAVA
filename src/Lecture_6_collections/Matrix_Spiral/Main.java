095d3dfdf280311db3f6eceb952a3b2bd7ff2803
Vasinca-Mihai <m_vasinca@yahoo.com>
2021-12-01 19:16:46 +0200

    corrected some problems with the spiral matrix program

--- src/Lecture_6_collections/Matrix_Spiral/Main.java
+++ src/Lecture_6_collections/Matrix_Spiral/Main.java
@@ -15,34 +15,64 @@ public class Main {
         spiralThrough(matrix);
         }
 
+    //public static void spiralThrough (List<List<Integer>> matrix){
+    //    String output = "";
+    //    while(matrix != null){
+    //        for(int i = 0;i<matrix.size();i++){
+    //            output += matrix.get(0).get(i) + ", ";
+    //        }
+    //        for(int i = 1;i<matrix.size();i++){
+    //            output += matrix.get(i).get(matrix.get(i).size()-1) + ", ";
+    //        }
+    //        for(int i = matrix.get(matrix.size()-1).size()-2;i>=0;i--){
+    //            output += matrix.get(matrix.size()-1).get(i) + ", ";
+    //        }
+    //        for(int i = matrix.size()-2;i>=1;i--){
+    //            output += matrix.get(i).get(0) + ", ";
+    //        }
+    //        if(matrix.size() <=2 || matrix.get(1).size() <=2) break;
+    //        matrix = removeBorders(matrix);
+    //    }
+    //    System.out.println(output);
+    //}
+
     public static void spiralThrough (List<List<Integer>> matrix){
         String output = "";
+        String middle = "";
+        if(matrix.size() % 2 == 1 && matrix.get(0).size() % 2 ==1){middle = matrix.get(matrix.size()/2).get(matrix.get(0).size()/2).toString();}
         while(matrix != null){
-            for(int i = 0;i<matrix.size();i++){
+            for(int i = 0;i<matrix.get(0).size()-1;i++){
                 output += matrix.get(0).get(i) + ", ";
             }
-            for(int i = 1;i<matrix.size();i++){
+            for(int i = 0;i<matrix.size()-1;i++){
                 output += matrix.get(i).get(matrix.get(i).size()-1) + ", ";
             }
-            for(int i = matrix.get(matrix.size()-1).size()-2;i>=0;i--){
+            for(int i = matrix.get(matrix.size()-1).size()-1;i>=1;i--){
                 output += matrix.get(matrix.size()-1).get(i) + ", ";
             }
-            for(int i = matrix.size()-2;i>=1;i--){
+            for(int i = matrix.size()-1;i>=1;i--){
                 output += matrix.get(i).get(0) + ", ";
             }
             if(matrix.size() <=2 || matrix.get(1).size() <=2) break;
             matrix = removeBorders(matrix);
         }
+        output += middle;
         System.out.println(output);
     }
 
     public static List<List<Integer>> removeBorders(List<List<Integer>> matrix){
         matrix.remove(0);
         matrix.remove(matrix.size()-1);
-        matrix.get(matrix.size()-1).remove(0);
-        matrix.get(matrix.size()-1).remove(matrix.get(matrix.size()-1).size()-1);
-        matrix.get(0).remove(0);
-        matrix.get(0).remove(matrix.get(0).size()-1);
+        for(int i = 0;i<matrix.size();i++){
+            matrix.get(i).remove(matrix.get(i).size()-1);
+        }
+        for(int i = 0;i<matrix.size();i++){
+            matrix.get(i).remove(0);
+        }
+        //matrix.get(matrix.size()-1).remove(0);
+        //matrix.get(matrix.size()-1).remove(matrix.get(matrix.size()-1).size()-1);
+        //matrix.get(0).remove(0);
+        //matrix.get(0).remove(matrix.get(0).size()-1);
         return matrix;
     }
 }
